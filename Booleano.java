4298a78968269b316265e754313a6ec81c090e30
Witless <witlesshydra@outlook.com>
2021-11-25 19:11:33 +0100

    Create Booleano.java

---
+++ Booleano.java
@@ -0,0 +1,15 @@
+public class Booleano {
+    private boolean valor;
+
+    Booleano(boolean valor){
+        this.valor = valor;
+    }
+
+    public void setValor(boolean valor) {
+        this.valor = valor;
+    }
+
+    public boolean getValor(){
+        return this.valor;
+    }
+}
