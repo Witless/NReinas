87855f924068231e0c1d095214557c24ff3c4ced
Witless <witlesshydra@outlook.com>
2021-11-25 19:11:16 +0100

    Create Entero.java

---
+++ Entero.java
@@ -0,0 +1,15 @@
+public class Entero {
+    private int valor;
+
+    Entero(int valor){
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
